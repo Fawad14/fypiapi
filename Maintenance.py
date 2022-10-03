0b7288eb198f4d74d032265e87518abf85a16754
Fawad14 <114941332+Fawad14@users.noreply.github.com>
2022-10-03 22:57:49 +0500

    Add files via upload

---
+++ Maintenance.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  2 02:53:12 2022
+
+@author: fawad
+"""
+
+from pydantic import BaseModel
+# 2. Class which describes Bank Notes measurements
+class Maintenance(BaseModel):
+    TEMPRATURE: float 
+    VOLTAGE: float 
+    current: float 
+    POWER: float
